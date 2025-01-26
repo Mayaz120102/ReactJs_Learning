cd5b954414f253f40db5cdbfe8ba55a9bfddbd6f
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-01-27 01:09:17 +0600

    learning about state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
       <Navbar title="Flagmanuing" About="About Me" />
+      <div className="container">
+        {" "}
+        <TextForm heading="enter the email and passward" />
+      </div>
     </>
   );
 }
