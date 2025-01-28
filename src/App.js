60737501a87d07bbda09111ffac4574b5fec563a
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-01-28 23:19:35 +0600

    toggle switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
       <Navbar title="Flagmanuing" About="About Me" />
       <div className="container">
         {" "}
-        <TextForm heading="enter the text to convert" />
+        {/* <TextForm heading="enter the text to convert" /> */}
+        <About />
       </div>
     </>
   );
