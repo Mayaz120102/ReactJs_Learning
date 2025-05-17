72db9bd192548ea1e120de831bc7d5151dc23254
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-05-17 11:20:37 +0600

    brandpage

---
+++ brand-page/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Brandpage from "./components/brandpage";
+import Nav from "./components/Nav";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Brandpage />
+    </div>
+  );
+};
+
+export default App;
