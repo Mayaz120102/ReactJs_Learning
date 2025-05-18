701fd20ea44406fbfe6a6b6f9719dd1ccbef3344
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-05-19 01:26:39 +0600

    button adding

--- Contact-Us/src/components/Button/Button.jsx
+++ Contact-Us/src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
-import { MdMessage } from "react-icons/md";
 import styles from "./Button.module.css";
 
-const Button = () => {
+const Button = (props) => {
   return (
     <button className={styles.primary_btn}>
-      <MdMessage /> VIA SUPPORT CHAT
+      {props.icon}
+      {props.text}
     </button>
   );
 };
--- Contact-Us/src/components/ContactForm/ContactForm.jsx
+++ Contact-Us/src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,17 @@
 import Button from "../Button/Button";
 import styles from "./Contact.module.css";
+import { MdMessage } from "react-icons/md";
+import { MdCall } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
 const ContactForm = () => {
   return (
-    <section>
+    <section className={styles.container}>
       <div className={styles.contact_form}>
-        <Button />
+        <div className={styles.top_btn}>
+          <Button text="VIA SUPPORT CHAT" icon={<MdMessage />} />
+          <Button text="VIA call" icon={<MdCall />} />
+        </div>
+        <Button isOutline={true} text="VIA EMAIl" icon={<MdEmail />} />
       </div>
       <div className={styles.contact_images}></div>
     </section>
