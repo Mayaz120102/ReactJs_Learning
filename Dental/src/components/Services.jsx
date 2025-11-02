6a3595df812f79dc5408d14679f1bd8a6f022737
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-11-02 13:23:59 +0600

    dental clinic

---
+++ Dental/src/components/Services.jsx
@@ -0,0 +1,32 @@
+import { icons } from "lucide-react";
+import React from "react";
+import { FaSmileBeam, FaTooth, FaXRay } from "react-icons/fa";
+import { GiToothbrush } from "react-icons/gi";
+
+const services = [
+  {
+    icon: <FaTooth className="w-10 h-10 text-sky-500" />,
+    title: "ldfsldfjdlfjsdlfjsdlfjsdlkfjsdf",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quis quae non repellendus quasi quidem.",
+  },
+  {
+    icon: <GiToothbrush className="w-10 h-10 text-green-500" />,
+    title: "ldfsldfjdlfjsdlfjsdlfjsdlkfjsdf",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quis quae non repellendus quasi quidem.",
+  },
+  {
+    icon: <FaSmileBeam className="w-10 h-10 text-yellow-500" />,
+    title: "ldfsldfjdlfjsdlfjsdlfjsdlkfjsdf",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quis quae non repellendus quasi quidem.",
+  },
+  {
+    icon: <FaXRay className="w-10 h-10 text-pink-500" />,
+    title: "ldfsldfjdlfjsdlfjsdlfjsdlkfjsdf",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quis quae non repellendus quasi quidem.",
+  },
+];
+const Services = () => {
+  return <div></div>;
+};
+
+export default Services;
