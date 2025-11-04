267d57ed486d3ad60c4113240f2937e7b8ee5eb2
Mayaz120102 <abrarmayaz2002@gmail.com>
2025-11-04 14:12:18 +0600

    updated app.jsx

--- Dental/src/App.jsx
+++ Dental/src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
+import About from "./components/About";
+import Tips from "./components/Tips";
+import AppoinmentBook from "./components/AppoinmentBook";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Services />
+      <About />
+      <Tips />
+      <AppoinmentBook />
+      <Testimonial />
+      <Footer />
     </div>
   );
 };
